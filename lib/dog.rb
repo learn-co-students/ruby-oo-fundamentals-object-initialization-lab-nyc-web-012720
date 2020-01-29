31144d4ab45f4f56fd538da604b1398f98be980b
github username <github email address>
2020-01-29 12:08:08 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+    def initialize(dog_name="Mutt" , breed="Mutt")
+        @name = dog_name
+        @breed = breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person
+    def initialize(person_name="Mutt")
+        @name = person_name
+    end
+end
